2321bca2a768093b4b124c49fed9e1edbc9e7e24
Aurelien Lourot <software@lourot.dev>
2023-11-10 15:29:58 +0100

    Add grommet

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Counter } from "./Counter";
 
+import { Grommet, Box, Text } from "grommet";
+
 export default function Page() {
   return (
-    <>
+    <Grommet
+      theme={{
+        global: {
+          colors: { doc: "#ff99cc" },
+        },
+      }}
+    >
       <h1>My Vike app</h1>
       This page is:
       <ul>
@@ -12,6 +20,9 @@ export default function Page() {
           Interactive. <Counter />
         </li>
       </ul>
-    </>
+      <Box pad="large" background="doc">
+        <Text color="red">Grommet box</Text>
+      </Box>
+    </Grommet>
   );
 }
